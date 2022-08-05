ad913b81b4da36443db45841f511797d887db0e1
Nick <nicolas.cinera@gmail.com>
2022-08-04 19:51:05 -0500

    Go Init 101

---
+++ learning-go/module1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	const fixedNum = 50 // 固定数字
+	var eventName = "LearningGOEvent"
+	fmt.Printf("This is the start of the %v \nGet your tickets now!", eventName)
+	// comments in Go are like this
+}
