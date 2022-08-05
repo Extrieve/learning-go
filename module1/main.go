e714375b16113b70a96a9510e569d67d4e677cff
Nick <nicolas.cinera@gmail.com>
2022-08-04 22:20:58 -0500

    Basic Input

--- module1/main.go
+++ module1/main.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	const fixedNum = 50 // 固定数字
-	var eventName = "LearningGOEvent"
+	const fixedNum = 50               // 固定数字
+	var eventName = "LearningGOEvent" // イベント名
+	var numberOfTickets int
+
 	fmt.Printf("This is the start of the %v \nGet your tickets now!", eventName)
-	// comments in Go are like this
+	// Printing out the types of my variables
+	fmt.Printf("Fixed Num: %T \nEvent Name: %T \nNumber ofTickets: %T\n", fixedNum, eventName, numberOfTickets)
+
+	fmt.Scan(&numberOfTickets) // チケット数を入力
+	fmt.Printf("You have purchased %v tickets\nTotal price will be: %v", numberOfTickets, (fixedNum * numberOfTickets))
 }
