0f99b39391b829a82e01eb9b1e0a0edfe7757b25
Nick <nicolas.cinera@gmail.com>
2022-08-10 21:27:34 -0500

    Simple Loop

--- module1/main.go
+++ module1/main.go
@@ -11,12 +11,15 @@ func main() {
 	var eventName = "LearningGOEvent" // イベント名
 	var numberOfTickets int
 
-	fmt.Printf("This is the start of the %v \nGet your tickets now!", eventName)
-	// Printing out the types of my variables
-	fmt.Printf("Fixed Num: %T \nEvent Name: %T \nNumber ofTickets: %T\n", fixedNum, eventName, numberOfTickets)
+	for {
 
-	fmt.Scan(&numberOfTickets) // チケット数を入力
-	fmt.Printf("You have purchased %v tickets\nTotal price will be: %v", numberOfTickets, (fixedNum * numberOfTickets))
+		fmt.Printf("This is the start of the %v \nGet your tickets now!", eventName)
+		// Printing out the types of my variables
+		fmt.Printf("Fixed Num: %T \nEvent Name: %T \nNumber ofTickets: %T\n", fixedNum, eventName, numberOfTickets)
 
-	fmt.Printf("The sum of %v and %v is %v\n", fixedNum, fixedNum, addTwoNumbers(fixedNum, numberOfTickets))
+		fmt.Scan(&numberOfTickets) // チケット数を入力
+		fmt.Printf("You have purchased %v tickets\nTotal price will be: %v", numberOfTickets, (fixedNum * numberOfTickets))
+
+		fmt.Printf("The sum of %v and %v is %v\n", fixedNum, fixedNum, addTwoNumbers(fixedNum, numberOfTickets))
+	}
 }
